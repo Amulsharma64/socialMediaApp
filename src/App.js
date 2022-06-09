86d4322b1ce8263156e5704812d680fb2d8bcdcc
Amul Sharma <94516019+Amulsharma64@users.noreply.github.com>
2022-06-09 17:39:59 +0530

    coomit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>Hello! Amul Sharma</h1>
+      {/* <h1>Hello! Amul Sharma</h1> */}
 
       <BrowserRouter>
         <Routes>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
       });
   });
   return (
-    <div className="name">
+    <div className="home">
       {posts.map((post, index) => (
         <div key={index}>{<Blog title={post.title} body={post.body} />}</div>
       ))}
