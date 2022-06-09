2050c5e0745cf63614ba27ea68d26f80e13734d2
Amul Sharma <94516019+Amulsharma64@users.noreply.github.com>
2022-06-09 17:45:25 +0530

    f

--- src/components/cards/BasicCard.jsx
+++ src/components/cards/BasicCard.jsx
@@ -7,15 +7,17 @@ import Typography from "@mui/material/Typography";
 
 export default function BasicCard({ title, body }) {
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ minWidth: 200 }}>
       <CardContent>
         {/* <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           Word of the Day!
         </Typography> */}
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        <Typography sx={{ mb: 1.5 }} variant="h5">
           {title}
         </Typography>
-        <Typography variant="body2">{body}</Typography>
+        <Typography variant="body2" color="text.secondary">
+          {body}
+        </Typography>
       </CardContent>
       <CardActions>
         <Button size="small">Edit</Button>
