017438901afd13dc4a3047f99dee76299c5d75d9
Amul Sharma <94516019+Amulsharma64@users.noreply.github.com>
2022-07-01 12:49:40 +0530

    Footer Added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,25 @@
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
 export default function Footer() {
+  const year = new Date().getFullYear().toString();
   return (
     <>
-      <InstagramIcon />
-      <FacebookIcon />
-      <LinkedInIcon />
+      <footer>
+        <div className="footer-text">
+          <span>
+            Copyright © {year} AKSARA Technologies Pvt. Ltd. All rights reserved
+          </span>
+        </div>
+        <div className="footer-icon">
+          <FacebookIcon />
+          <InstagramIcon />
+          <LinkedInIcon />
+          <TwitterIcon />
+        </div>
+      </footer>
     </>
   );
 }
